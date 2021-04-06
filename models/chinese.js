9b9a3324d01c9f293549a310f02a25858321c96b
avijasoliya <64353343+avijasoliya@users.noreply.github.com>
2021-04-06 14:37:23 +0530

    Add files via upload

---
+++ models/chinese.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chineseSchema = new Schema(
+  {
+   
+    name: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    price:{
+      type:Number,
+      required:true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    feedback:String,
+    creator: {
+      type: Object,
+      // required: String
+    },
+    
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Chinese', chineseSchema);
